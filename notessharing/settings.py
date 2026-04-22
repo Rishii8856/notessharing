eb40f9898e2ea6f9a8ea2224cd14429702314c62
Rishiii <rushikeshjadhav568656@gmail.com>
2026-04-23 03:44:36 +0530

    final fix gunicorn + whitenoise

--- notessharing/settings.py
+++ notessharing/settings.py
@@ -108,10 +108,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 
