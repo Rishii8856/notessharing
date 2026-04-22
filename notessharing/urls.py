ece4993fc6dfe76b79409f2ab928aa60fff16c32
Rishiii <rushikeshjadhav568656@gmail.com>
2026-04-23 04:51:52 +0530

    fix notes url lowercase

--- notessharing/urls.py
+++ notessharing/urls.py
@@ -1,25 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
 
     path('', views.Index, name='index'),
 
-    # 👉 ONLY ONE notes route (FINAL)
-    path('notes/', views.NOTES_DETAILS, name='notes'),
-
-    # बाकी routes
     path('login/', views.LOGIN, name='login'),
     path('dologin/', views.doLogin, name='doLogin'),
     path('logout/', views.doLogout, name='logout'),
 
-    path('', include('nssapp.urls')),  # other app routes
-]
+    # 🔥 IMPORTANT FIX
+    path('notes/', views.NOTES_DETAILS, name='notes'),
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('nssapp.urls')),
+]
